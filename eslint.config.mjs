d1908957efc712ed041e2a2e8af2db9dbaaf6beb
Dima Nikitiuk <freshwtf13@gmail.com>
2025-05-31 00:54:15 +0300

    Refactor ESLint configuration to spread recommended settings and improve readability

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,6 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 export default defineConfig([
   { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], plugins: { js }, extends: ['js/recommended'] },
   { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  eslintConfigPrettier,
+  ...tseslint.configs.recommended,
+  {files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], ...eslintConfigPrettier}
 ]);
